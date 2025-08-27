b4e0717d25100e0636c2e8b12bf8c9b64ae9ea15
Pang Jun Meng <oscarpjmgt@gmail.com>
2025-08-28 01:27:01 +0800

    Until 7) OrderClient (internal validation)

---
+++ KFC-Food-Ordering-and-Management-System/app/Payments/Handlers/PaymentHandlerInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Payments\Handlers;
+
+interface PaymentHandlerInterface
+{
+    /**
+     * Process payment (synchronous)
+     *
+     * @param array $data ['payment' => PaymentModel, 'payload' => array]
+     * @return array ['success' => bool, 'transactionRef' => string|null, 'message' => string|null]
+     */
+    public function pay(array $data): array;
+
+    /**
+     * Refund payment
+     * @param string $transactionRef
+     * @return array ['success' => bool, 'message' => string|null]
+     */
+    public function refund(string $transactionRef): array;
+}
